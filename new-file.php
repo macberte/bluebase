f6016bfab523a3f36aca1d6c0c0a06097d844e1b
macberte <macberte@gmail.com>
2025-02-15 17:40:49 +0100

    Aggiunto file NEW-File.php

---
+++ new-file.php
@@ -0,0 +1,2 @@
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat illo eum aliquam, et iure modi molestiae animi in
+suscipit ipsum, saepe qui laboriosam! Nobis reiciendis excepturi perspiciatis eos est expedita.
