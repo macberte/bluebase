8c550a6448cc43327a7199b3a0ffd6d3269a2ee3
macberte <macberte@gmail.com>
2025-02-15 16:55:21 +0100

    Aggiunti file Test + Test2

---
+++ TEST-2.php
@@ -0,0 +1,17 @@
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore debitis voluptate, recusandae placeat in veritatis
+error illo quo consectetur, vero dolorem adipisci nam? Natus alias placeat fugiat nesciunt eligendi quo.
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
