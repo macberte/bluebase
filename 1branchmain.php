6218d10503ec84aad70f8b5b73705a8c2f5f3d45
macberte <macberte@gmail.com>
2025-02-15 18:03:50 +0100

    Ripristino condizioni iniziali del tema

--- 1branchmain.php
+++
@@ -1,4 +0,0 @@
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus unde deleniti, qui animi ratione, id temporibus
-tempore maxime repudiandae commodi nobis natus delectus eligendi praesentium? Quas architecto eaque et accusamus.
-
-Aggiungo testo in main
